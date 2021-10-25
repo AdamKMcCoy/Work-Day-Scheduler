5f9c501b7f4d36fe22ac8253b5808b9d2f4e66e1
Adam McCoy <82851002+AdamKMcCoy@users.noreply.github.com>
2021-10-24 21:23:20 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Work Day Scheduler
+Third-Party APIs Challenge
+
+## Discription 
+A simple planner or calendar that allows a user to save events for each hour of the day. When the user opens the application the current day is displayed at the top of the calendar.
+
+## Built With
+* HTML
+* CSS
+* Javascript
+* jQuery
+* Bootstrap
+* Moment.js
+* Font Awesome
+
+## Website
---
+++ assets/script.js
@@ -0,0 +1,43 @@
+  ("#7am .description").val(localStorage.getItem("7am"));
+  ("#8am .description").val(localStorage.getItem("8am"));
+  ("#9am .description").val(localStorage.getItem("9am"));
+  ("#10am .description").val(localStorage.getItem("10am"));
+  ("#11am .description").val(localStorage.getItem("11am"));
+  ("#12pm .description").val(localStorage.getItem("12pm"));
+  ("#1pm .description").val(localStorage.getItem("1pm"));
+  ("#2pm .description").val(localStorage.getItem("2pm"));
+  ("#3pm .description").val(localStorage.getItem("3pm"));
+  ("#4pm .description").val(localStorage.getItem("4pm"));
+  ("#5pm .description").val(localStorage.getItem("5pm"));
+(document).ready(function() {
+    var timeUpdate = function() {
+         ("#currentDate").text(moment().format("dddd, MMMM Do h:mm:ss a"));
+         ("#currentTime").text(moment().format("h:mm:ss a"));
+    }
+    timeUpdate();
+    setInterval(timeUpdate, 1000);
+    var colorCoder = function() {
+        var presentTime = moment().hours();
+         (".time-block").each(function() {
+        var timeBlock = parseInt( (this).attr("id")[1]);
+        if (timeBlock < presentTime) {
+           (this).addClass("past");
+        } 
+        else if (timeBlock === presentTime) {
+           (this).addClass("present");
+        } 
+        else {
+           (this).addClass("future");
+        }
+        console.log(timeBlock);
+      })
+    }
+    colorCoder();
+     (".saveBtn").on("click", function() {
+        var taskEntryTime =  (this).parent().attr("id");
+        var taskEntryText =  (this).siblings(".description").val();
+        localStorage.setItem(taskEntryTime, taskEntryText);
+        console.log(taskEntryTime, taskEntryText);
+    })
+}
+)
